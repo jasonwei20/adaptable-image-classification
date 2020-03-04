44d04ef6f04aea76d802747f6e0ee89237ef1f92
Jason Wei <jason.weng.wei@gmail.com>
2020-03-03 22:07:25 -0500

    fix aliases and reformat train

--- vanilla/53_vanilla_train_voc.py
+++ vanilla/53_vanilla_train_voc.py
@@ -6,10 +6,13 @@ from utils_model import train_resnet
 
 exp_num = 53
 
+#manual
 train_folder = Path("/home/brenta/scratch/jason/data/voc/voc_trainval_full/train")
 val_folder = Path("/home/brenta/scratch/jason/data/voc/voc_trainval_full/val")
 checkpoints_folder = Path("/home/brenta/scratch/jason/checkpoints/voc/vanilla/exp_" + str(exp_num))
 log_folder = Path("/home/brenta/scratch/jason/logs/voc/vanilla/exp_" + str(exp_num))
+
+#self-generated
 log_csv = get_log_csv_name(log_folder=log_folder)
 train_order_csv = get_log_csv_train_order(log_folder=log_folder)
 classes = get_classes(train_folder)
@@ -24,6 +27,6 @@ train_resnet(   train_folder = train_folder,
                 classes = classes,
                 num_classes = num_classes,
                 device = device, 
-                save_mb_interval=50,
-                val_mb_interval=50,
+                save_mb_interval = 50,
+                val_mb_interval = 50,
                 )
